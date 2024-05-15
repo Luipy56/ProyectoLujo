e31a52562a42afa1ef7f5590181d6c64197e19dc
Luipy <yoelberjaga@gmail.com>
2024-05-15 03:47:11 +0200

    Reestructurada La Web

---
+++ htmlZero/cambiarTitulo.js
@@ -0,0 +1,20 @@
+var frases = [
+  "¡No te vayas 😢!",
+  "¡No te vayas 😢!",
+  "Tonto el que lo lea 😢",
+  "¿Seguro que quieres irte? 😢",
+  "¡Espera! Hay más 😢",
+  "¡Vuelve 😢!",
+  "¡Vuelve 😢!"
+];
+function fraseAleatoria() {
+    return frases[Math.floor(Math.random() * frases.length)];
+}
+
+document.addEventListener("visibilitychange", function() {
+  if (document.visibilityState === 'hidden') {
+    document.title = fraseAleatoria();"¡Vuelve 😢!";
+  } else {
+    document.title = "Corporación Lujo";
+  }
+});
